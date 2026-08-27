c7ef92bd650bedb1c3ad684d3f72dcd9dae8064d
Avnish Chauhan <avnish.chauhan.cs27@iilm.edu>
2026-08-27 10:45:59 +0530

    Time: 2 ms (89.59%), Space: 44.1 MB (56.24%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,51 +1,46 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-           int m = grid.length, n = grid[0].length;
-
-        Queue<int[]> queue = new LinkedList<>();
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
         int fresh = 0;
-
-        // Step 1: initialize queue and count fresh
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 2) {
-                    queue.offer(new int[]{i, j});
-                } else if (grid[i][j] == 1) {
+                    q.offer(new int[]{i, j});
+                }
+                if (grid[i][j] == 1) {
                     fresh++;
                 }
             }
         }
-
-        // If no fresh oranges
-        if (fresh == 0) return 0;
-
         int minutes = 0;
-        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
-
-        // BFS
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            boolean rottedThisRound = false;
-
+        int[][] directions = {
+            {-1, 0},  
+            {1, 0},  
+            {0, -1},  
+            {0, 1}    
+        };
+        while (!q.isEmpty() && fresh > 0) {
+            int size = q.size();
             for (int i = 0; i < size; i++) {
-                int[] curr = queue.poll();
-
-                for (int[] d : dir) {
-                    int x = curr[0] + d[0];
-                    int y = curr[1] + d[1];
-
-                    if (x >= 0 && y >= 0 && x < m && y < n && grid[x][y] == 1) {
-                        grid[x][y] = 2;
-                        queue.offer(new int[]{x, y});
+                int[] curr = q.poll();
+                int r = curr[0];
+                int c = curr[1];
+                for (int[] dir : directions) {
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+                    if (nr >= 0 && nr < m &&
+                        nc >= 0 && nc < n &&
+                        grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;
                         fresh--;
-                        rottedThisRound = true;
+                        q.offer(new int[]{nr, nc});
                     }
                 }
             }
-
-            if (rottedThisRound) minutes++;
+            minutes++;
         }
-
         return fresh == 0 ? minutes : -1;
     }
 }
