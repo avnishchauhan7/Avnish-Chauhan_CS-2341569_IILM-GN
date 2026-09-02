ca7fb9e0a4a97595e9b2d856f8d034aef7f7c652
Avnish Chauhan <avnish.chauhan.cs27@iilm.edu>
2026-09-02 09:26:48 +0530

    Time: 0 ms (100%), Space: 42 MB (58.88%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countOrders(int n) {
+        long ans = 1;
+        long mod = 1000000007;
+        for (int i = 1; i <= n; i++) {
+            ans = ans * i % mod;
+            ans = ans * (2L * i - 1) % mod;
+        }
+        return (int) ans;
+    }
+}
